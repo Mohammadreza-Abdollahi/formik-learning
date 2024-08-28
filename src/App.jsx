dd04e6ecdd41cbc40bef02b90385b59b1217d7f1
Mohammadreza Abdollahi <mohammad1384abdollahi@gmail.com>
2024-08-28 15:21:46 +0330

    version 0.0.1
    install plugins

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = ()=>{
+  return (
+    <>
+      <h1 className="text-5xl text-green-600">Helloo World</h1>
+      <h1 className="text-5xl text-green-600">Mohammadreza</h1>
+    </>
+  );
+}
+
+export default App;
